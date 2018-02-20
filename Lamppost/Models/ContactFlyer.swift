97dd2526d623ecbf47abfdd020a77f53e1df6396
Spencer Edgecombe <se185066@wcajc185196-02y.local>
2018-02-20 12:35:13 -0500

    Actually fix that bug.

--- Lamppost/Models/ContactFlyer.swift
+++ Lamppost/Models/ContactFlyer.swift
@@ -77,21 +77,41 @@ class ContactFlyer : Flyer {
     
     private func performCallAction(action : ContactAction) {
         
-        let number = action.value.replacingOccurrences(of: "(", with: "").replacingOccurrences(of: ")", with: "").replacingOccurrences(of: "-", with: "").replacingOccurrences(of: " ", with: "")
+        var number = phoneNumberUrl(number: action.value)
+    
         if let url = URL(string: "tel://\(number)"), UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        } else {
+            print(phoneNumberUrl(number: action.value))
+        }
+    }
+    
+    private func performMessageAction(action : ContactAction) {
+        let number = phoneNumberUrl(number: action.value)
+        
+        if let url = URL(string: "sms://\(number)"), UIApplication.shared.canOpenURL(url) {
             UIApplication.shared.open(url)
         } else {
             
         }
     }
     
-    private func performMessageAction(action : ContactAction) {
-        let number = action.value.replacingOccurrences(of: "(", with: "").replacingOccurrences(of: ")", with: "").replacingOccurrences(of: "-", with: "").replacingOccurrences(of: " ", with: "")
-        if let url = URL(string: "sms://\(number)"), UIApplication.shared.canOpenURL(url) {
-            UIApplication.shared.open(url)
-        } else {
+    private func phoneNumberUrl(number : String) -> String{
+        
+        var result = number.replacingOccurrences(of: "(", with: "").replacingOccurrences(of: ")", with: "").replacingOccurrences(of: "-", with: "").replacingOccurrences(of: " ", with: "")
+        
+        var i = 0
+        
+        while i < result.characters.count {
+            var c = result[result.index(result.startIndex, offsetBy: i)]
             
+            if (c != "0" && c != "1" && c != "2" && c != "3" && c != "4" && c != "5" && c != "6" && c != "7" && c != "8" && c != "9" && c != "+") {
+                result = result.substring(to: result.index(result.startIndex, offsetBy: i)) + result.substring(from: result.index(result.startIndex, offsetBy: i+1))
+            }
+            i = i + 1
         }
+        
+        return result
     }
     
     private func performEmailAction(action : ContactAction) {
