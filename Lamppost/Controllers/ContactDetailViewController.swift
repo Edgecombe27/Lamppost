72060cf3e08f3029a45e5ff671b522972d0e5f08
Edgecombe27 <edge1190@mylaurier.ca>
2018-05-09 18:28:52 -0400

    Update contact ui

--- Lamppost/Controllers/ContactDetailViewController.swift
+++ Lamppost/Controllers/ContactDetailViewController.swift
@@ -8,20 +8,31 @@
 
 import UIKit
 
-class ContactDetailViewController: UIViewController{
+class ContactDetailViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
 
+    @IBOutlet var collectionView: UICollectionView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var contentView: UIView!
     
     var viewController : ViewController!
     var flyer : ContactFlyer!
+    var actions : [String : ContactAction] = [:]
+    var labels : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        //contentView.layer.cornerRadius = 20
+        collectionView.register(UINib(nibName: ActionCellView.NIB_NAME, bundle: nil), forCellWithReuseIdentifier: ActionCellView.IDENTIFIER)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width:110,height: 110)
+        flowLayout.sectionInset = UIEdgeInsetsMake(0, 10, 0, 10)
+        flowLayout.scrollDirection = UICollectionViewScrollDirection.horizontal
+        flowLayout.minimumInteritemSpacing = 0.0
+        collectionView.collectionViewLayout = flowLayout
+        
         contentView.layer.masksToBounds = true
         
         imageView.image = flyer.icon
@@ -33,9 +44,48 @@ class ContactDetailViewController: UIViewController{
             nameLabel.text = nameLabel.text! + " (\(flyer.details["nickname"] as! String))"
         }
         
+        for action in flyer.getActions() as! [ContactAction] {
+            if action.type == flyer.CALL_ACTION {
+                actions["Call"] = action
+                labels.append("Call")
+                actions["SMS"] = ContactAction(type: flyer.MESSAGE_ACTION, label: "", value: action.value)
+                labels.append("SMS")
+            } else if action.type == flyer.EMAIL_ACTION {
+                actions["Email"] = action
+                labels.append("Email")
+            }
+        }
+        
+        labels.sort { (s1, s2) -> Bool in
+            if s1 == "Call" {
+                return true
+            } else if s1 == "SMS" && s2 == "Call" {
+                return false
+            } else if s1 == "Email" {
+                return false
+            } else {
+                return true
+            }
+        }
         
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return actions.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell : ActionCellView = collectionView.dequeueReusableCell(withReuseIdentifier: ActionCellView.IDENTIFIER, for: indexPath) as! ActionCellView
+        cell.render(withLabel: labels[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let label = labels[indexPath.row]
+        flyer.performAction(action: actions[label]!)
+    }
     
     @IBAction func exitViewTapped(_ sender: Any) {
         viewController.blurrView.isHidden = true
---
+++ Lamppost/Views/ActionCellView.swift
@@ -0,0 +1,30 @@
+//
+//  ActionCellView.swift
+//  kiOSK
+//
+//  Created by Spencer Edgecombe on 2018-05-09.
+//  Copyright © 2018 Lamppost. All rights reserved.
+//
+
+import UIKit
+
+class ActionCellView: UICollectionViewCell {
+
+    @IBOutlet var label: UILabel!
+    
+    static let NIB_NAME = "ActionCellView"
+    static let IDENTIFIER = "action_cell"
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        label.layer.cornerRadius = label.frame.height / 2.0
+        label.layer.masksToBounds = true
+    }
+    
+    func render(withLabel : String) {
+        label.text = withLabel
+    }
+
+}
